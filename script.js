c37267a63d4ab5ce404b03cb505eb3872ed5dc54
ReyChancla <61477102+ReyChancla@users.noreply.github.com>
2022-02-13 21:12:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function datosMartin() {
 // Esta es la función primaria que elige el tipo de caja
 
 function CalcularDatosMartin() {
-    const {m200, m201, m203} = datosMartin();
+    const {m200, m203} = datosMartin();
 
     if (m200) {
         CalcularDatosMartin200();   
@@ -88,25 +88,97 @@ function CalcularDatosMartin201() {
         const {m200, m201, m203, canalesMartin, largo, ancho, alto, costeCliche, NumeroCliches} = datosMartin();
         const {formatofecha} = fecha();
         const {nombre, valoresDiferenciaMartin: {c1, c2, s}} = canalesB1;
+        
+        
         if (nombre === canalesMartin) {
             const Tlargo = largo + c1;
             const Tancho = ancho + c1;
             const Talto = alto + c2;
             const pestaña = s;
 
+            const L = (((Tancho + Tlargo)*2)+pestaña);
+            const H = ((Tancho + Tlargo)*2);
+            const I = Talto + Tancho + c1;
+            const B1Cerrada = Tlargo + Tancho;
+            const E = ancho/2;
+            const F = Talto;
+
+            if (L >= 800 && L <= 2400) {
+                console.log("valor L correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            ///////////////////////////////////////////////////////////////
+            if (H >= 775 && H <= 2200) {
+                console.log("valor H correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            //////////////////////////////////////////////////////
+            if (I >= 300 && I <= 1000) {
+                console.log("valor I correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            //////////////////////////////////////////////////////
+            if (Tancho >= 120 && Tancho <= 950) {
+                console.log("valor ancho correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            /////////////////////////////////////////////////////////
+            if (Tlargo >= 250 && Tlargo <= 1080) {
+                console.log("valor largo correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            /////////////////////////////////////////////////////////////
+            if (B1Cerrada >= 400 && B1Cerrada <= 1100) {
+                console.log("valor caja cerrada correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            ///////////////////////////////////////////////////////////////
+            if (E >= 0 && E <= 300) {
+                console.log("valor E correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            ////////////////////////////////////////////////////////////////////
+            if (F >= 100 && F <= 800) {
+                console.log("valor F correcto");
+            } else{
+                document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+                document.getElementById("errorMedidas").style.background = "red";
+            };
+            
+
+
             document.getElementById("aparecer").style.opacity = "1";
             document.getElementById("elemento1").innerHTML = ("  ") + formatofecha;
             document.getElementById("elemento2").innerHTML = ("  ") + largo + (" x ") + ancho + (" x ") + alto;
-            document.getElementById("elemento3").innerHTML = ("  ") + (((Tancho + Tlargo)*2)+s) + (" x ") + (Talto + Tancho +c1);
+            document.getElementById("elemento3").innerHTML = ("  ") + L +(" x ")+ I ;
             document.getElementById("elemento4").innerHTML = ("  Ond / Martin"); 
             document.getElementById("elemento5").innerHTML = ("  ") + (((((Tancho + Tlargo)*2) * (Talto + Tancho +c1)*(costeCliche/100))/1000)*NumeroCliches).toFixed(2) + (" €");
-            document.getElementById("elemento6").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
-            document.getElementById("elemento7").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
-            document.getElementById("elemento8").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
+            document.getElementById("elemento6").innerHTML = ("  ") + NumeroCliches;
+            // document.getElementById("elemento7").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
+            // document.getElementById("elemento8").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
+            
         }
     }); 
 };
 
 function CalcularDatosMartin200() {
-   alert("Comprar cebollas"); 
+   alert("El atractivo programador necesita más tiempo"); 
 };
+
+function CalcularDatosMartin203() {
+    alert("Conformate ahora mismo con hacer B1"); 
+ };
