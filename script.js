632a195061d665a3377bdc792f61d0a53b29ff11
ReyChancla <61477102+ReyChancla@users.noreply.github.com>
2022-02-23 20:47:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,13 +36,17 @@ const DatosCanalesMartin = [
 // Datos ontenidos de los diferentes Input de la web
 function datosMartin() {
     const m200 = document.getElementById("m200").checked;
+    const m201 = document.getElementById("m201").checked;
     const m203 = document.getElementById("m203").checked;
     const canalesMartin = document.getElementById("canalesMartin").value;
     const largo = Number(document.getElementById("largoMartin").value);
     const ancho =Number(document.getElementById("anchoMartin").value);
     const alto = Number(document.getElementById("altoMartin").value);
     const costeCliche = parseInt(document.getElementById("Proveedor").value);
-    const NumeroCliches = parseInt(document.getElementById("numerodetintas").value);
+    const NumeroCliches = parseInt(document.getElementById("numerodetintas").value); const SelectGrade = DatosCanalesMartin.find(grade => {
+        return grade.nombre === canalesMartin;
+    });
+    const {nombre, valoresDiferenciaMartin: {c1, c2, s}} = SelectGrade;
     return{
         m200,
         m203,
@@ -51,13 +55,16 @@ function datosMartin() {
         ancho,
         alto,
         costeCliche,
-        NumeroCliches
+        NumeroCliches,
+        c1,
+        c2,
+        s
     }
 };
 // Esta es la función primaria que elige el tipo de caja
 
 function CalcularDatosMartin() {
-    const {m200, m203} = datosMartin();
+    const {m200, m201,  m203} = datosMartin();
 
     if (m200) {
         CalcularDatosMartin200();   
@@ -83,14 +90,107 @@ function fecha() {
 //Parte 1 B1
 
 function CalcularDatosMartin201() {
+    
+    const {canalesMartin, largo, ancho, alto, costeCliche, NumeroCliches, c1, c2, s} = datosMartin();
+    const {formatofecha} = fecha();
+    const Tlargo = largo + c1;
+    const Tancho = ancho + c1;
+    const Talto = alto + c2;
+    const pestaña = s;
+    const L = (((Tancho + Tlargo)*2)+pestaña);
+    const H = ((Tancho + Tlargo)*2);
+    const I = Talto + Tancho + c1;
+    const B1Cerrada = Tlargo + Tancho;
+    const E = ancho/2;
+    const F = Talto;
 
-    const xl = DatosCanalesMartin.forEach(canalesB1 => {
-        const {m200, m201, m203, canalesMartin, largo, ancho, alto, costeCliche, NumeroCliches} = datosMartin();
-        const {formatofecha} = fecha();
-        const {nombre, valoresDiferenciaMartin: {c1, c2, s}} = canalesB1;
+        if (L >= 800 && L <= 2400) {
+           
+        } else{
+            console.log("valor L incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        ///////////////////////////////////////////////////////////////
+        if (H >= 775 && H <= 2200) {
+            
+        } else{
+            console.log("valor H incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        //////////////////////////////////////////////////////
+        if (I >= 300 && I <= 900) {
+            
+        } else if (condition) {
+            
+        }{
+            console.log("valor I incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        //////////////////////////////////////////////////////
+        if (Tancho >= 120 && Tancho <= 950) {
+            
+        } else{
+            console.log("valor ancho incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        /////////////////////////////////////////////////////////
+        if (Tlargo >= 250 && Tlargo <= 1080) {
+
+        } else{
+            console.log("valor largo incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        /////////////////////////////////////////////////////////////
+        if (B1Cerrada >= 400 && B1Cerrada <= 1100) {
+            
+        } else{
+            console.log("valor caja cerrada incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        ///////////////////////////////////////////////////////////////
+        if (E >= 0 && E <= 300) {
+            
+        } else{
+            console.log("valor E incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
+        ////////////////////////////////////////////////////////////////////
+        if (F >= 100 && F <= 800) {
+            
+        } else{
+            console.log("valor F incorrecto");
+            document.getElementById("errorMedidas").innerHTML = ("Esta caja no se puede fabricar, por medidas.");
+            document.getElementById("errorMedidas").style.background = "red";
+        };
         
-        
-        if (nombre === canalesMartin) {
+        //Alerta de error
+        document.getElementById("aparecer").style.opacity = "1";
+        //Fecha
+        document.getElementById("elemento1").innerHTML = ("  ") + formatofecha;
+       //Medidas Internas
+        document.getElementById("elemento2").innerHTML = ("  ") + largo + (" x ") + ancho + (" x ") + alto;
+       // Medidas de Plancha
+        document.getElementById("elemento3").innerHTML = ("  ") + I +(" x ")+ L ;
+       //Ruta
+        document.getElementById("elemento4").innerHTML = ("  Ond / Martin"); 
+       //Coste del cliche
+        document.getElementById("elemento5").innerHTML = ("  ") + (((((Tancho + Tlargo)*2) * (Talto + Tancho +c1)*(costeCliche/100))/1000)*NumeroCliches).toFixed(2) + (" €");
+       //Numero de tintas
+        document.getElementById("elemento6").innerHTML = ("  ") + NumeroCliches;
+        // document.getElementById("elemento7").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
+        // document.getElementById("elemento8").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
+       
+}
+
+
+/*         if (nombre === canalesMartin) {
             const Tlargo = largo + c1;
             const Tancho = ancho + c1;
             const Talto = alto + c2;
@@ -160,25 +260,31 @@ function CalcularDatosMartin201() {
             };
             
 
-
+            //Alerta de error
             document.getElementById("aparecer").style.opacity = "1";
+            //Fecha
             document.getElementById("elemento1").innerHTML = ("  ") + formatofecha;
+           //Medidas Internas
             document.getElementById("elemento2").innerHTML = ("  ") + largo + (" x ") + ancho + (" x ") + alto;
+           // Medidas de Plancha
             document.getElementById("elemento3").innerHTML = ("  ") + L +(" x ")+ I ;
+           //Ruta
             document.getElementById("elemento4").innerHTML = ("  Ond / Martin"); 
+           //Coste del cliche
             document.getElementById("elemento5").innerHTML = ("  ") + (((((Tancho + Tlargo)*2) * (Talto + Tancho +c1)*(costeCliche/100))/1000)*NumeroCliches).toFixed(2) + (" €");
+           //Numero de tintas
             document.getElementById("elemento6").innerHTML = ("  ") + NumeroCliches;
             // document.getElementById("elemento7").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
             // document.getElementById("elemento8").innerHTML = ("  ") + Tancho + (" x ") + Tlargo + (" x ") + Talto;
             
         }
     }); 
-};
+};*/
 
 function CalcularDatosMartin200() {
    alert("El atractivo programador necesita más tiempo"); 
 };
-
+ 
 function CalcularDatosMartin203() {
     alert("Conformate ahora mismo con hacer B1"); 
  };
